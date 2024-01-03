4f616ce5446fd807113c56963706fee5fc48121e
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-01-02 17:15:53 -0800

    Server boilerplate

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const db = require('./config/connection.js');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log('DB connection successful at', process.env.DB_CONN_URL);
+        console.log('Express server listening on port', process.env.SERVER_PORT);
+    });
+});
