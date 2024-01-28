28d0e081f74f0d951b5f879e81d3560d3b185e13
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-01-27 16:23:05 -0800

    Utility commit (preparing for build)

--- server/server.js
+++ server/server.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use(routes);
 app.use(err);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+}
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
+
 db.once('open', async () => {
     app.listen(process.env.SERVER_PORT, async () => {
         console.log('DB connection successful');
